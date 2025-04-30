84aac9fcf4e69247ef29e77dbe3deadd4475989d
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-04-30 14:53:39 -0300

    el token devuelve el id y rol

--- src/services/AuthServices.mjs
+++ src/services/AuthServices.mjs
@@ -75,7 +75,7 @@ class AuthServices extends IRepository {
         }
     
         generateToken(user){
-            return jwt.sign({ id: user._id, username: user.username}, process.env.JWT_SECRET, {expiresIn: '24h'})
+            return jwt.sign({ id: user._id, role: user.role}, process.env.JWT_SECRET, {expiresIn: '24h'})
         }
 }
 
